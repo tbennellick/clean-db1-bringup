3a898563042db57f57c2c3e84290ef462c71ee85
Tom Bennellick <tom@bennellick.com>
2025-05-07 16:56:01 +0100

    Add custom board for dev-board

--- app/src/main.c
+++ app/src/main.c
@@ -1,14 +1,17 @@
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
 #include <zephyr/logging/log.h>
 
 #include <app_version.h>
+
+#include "debug_leds.h"
 
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
 int main(void)
 {
 	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
+    init_debug_leds();
+    debug_led_on();
 
 	while (1)
         {
