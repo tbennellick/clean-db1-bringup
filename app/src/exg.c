888740f8311550111562f689448294aee038ebe0
Tom Bennellick <tom@bennellick.com>
2025-05-23 13:42:10 +0100

    Hack ExG CS to external GPIO.
    It seems that the NXP driver does not support SPI_HOLD_ON_CS.
    The CS needs to be held high for at least for clok cycles.
    
    If this is the way to go, the GPIO will be passsed in via device tree.

--- app/src/exg.c
+++ app/src/exg.c
@@ -14,6 +14,10 @@ static const struct gpio_dt_spec exg_clksel = GPIO_DT_SPEC_GET(EXG_CLKSEL, gpios
 static const struct gpio_dt_spec exg_nrst = GPIO_DT_SPEC_GET(EXG_NRST, gpios);
 #define EXG_NPWDN DT_ALIAS(exg_npwdn)
 static const struct gpio_dt_spec exg_npwdn = GPIO_DT_SPEC_GET(EXG_NPWDN, gpios);
+
+#define EXG_CS_TEMP DT_ALIAS(exg_cs_temp)
+static const struct gpio_dt_spec exg_cs_temp = GPIO_DT_SPEC_GET(EXG_CS_TEMP, gpios);
+
 void init_exg(void)
 {
     const struct device *const dev = DEVICE_DT_GET_ONE(ti_ads1298);
@@ -21,7 +25,7 @@ void init_exg(void)
     safe_init_gpio(&exg_clksel, GPIO_OUTPUT_HIGH);
     safe_init_gpio(&exg_nrst, GPIO_OUTPUT_HIGH);
     safe_init_gpio(&exg_npwdn, GPIO_OUTPUT_HIGH);
-
+    safe_init_gpio(&exg_cs_temp, GPIO_OUTPUT_HIGH);
 	if (!device_is_ready(dev)) {
         LOG_ERR("Device %s is not ready\n", dev->name);
         k_sleep(K_MSEC(200));
@@ -33,7 +37,10 @@ void init_exg(void)
     while (1)
     {
 
+        gpio_pin_set_dt(&exg_cs_temp, 0);
         sensor_sample_fetch_chan(dev, SENSOR_CHAN_PRESS);
+        gpio_pin_set_dt(&exg_cs_temp, 1);
+
         sensor_channel_get(dev, SENSOR_CHAN_PRESS, &p);
 
         LOG_INF("pressure: %d.%06d\n", p.val1, p.val2);
