5d61b66979ff5d55b8b82a52061a096c2fa7ee59
Tom Bennellick <tom@bennellick.com>
2025-06-05 11:03:31 +0100

    Implement pressure sensor status command.

--- drivers/sensor/abp2s/abp2s.c
+++ drivers/sensor/abp2s/abp2s.c
@@ -10,29 +10,64 @@
 #include "abp2s.h"
 
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(ABP2S, CONFIG_SENSOR_LOG_LEVEL);
+//LOG_MODULE_REGISTER(ABP2S, CONFIG_SENSOR_LOG_LEVEL);
+LOG_MODULE_REGISTER(ABP2S, LOG_LEVEL_DBG);
+
+#define ABP2S_CMD_NOP 0xF0
+#define ABP2S_CMD_MEASURE 0xAA
+#define ABP2S_MAX_TRX_LEN 7
+
+uint8_t rx_bytes[ABP2S_MAX_TRX_LEN] = {0};
+struct spi_buf rx_buf = { .buf = rx_bytes, .len = sizeof(rx_bytes) };
+const struct spi_buf_set pressure_rx_set = { .buffers = &rx_buf, .count = 1 };
+
+uint8_t tx_bytes[ABP2S_MAX_TRX_LEN] = {0};
+struct spi_buf tx_buf = { .buf = tx_bytes, .len = sizeof(tx_bytes) };
+const struct spi_buf_set pressure_tx_set = { .buffers = &tx_buf, .count = 1 };
 
 
-static int abp2_read(const struct device *dev, uint8_t *val)
+//
+//static int abp2_read(const struct device *dev, uint8_t *val)
+//{
+//	const struct abp2_dev_config *cfg = dev->config;
+//
+//	uint8_t cmd_buf[7] = {0};
+//
+//	const struct spi_buf rx_buf = { .buf = cmd_buf, .len = sizeof(cmd_buf) };
+//	const struct spi_buf_set rx = { .buffers = &rx_buf, .count = 1 };
+//
+//	int ret = spi_read_dt(&cfg->bus, &rx);
+//
+//	if (ret !=0) {
+//        LOG_ERR("Failed to read from SPI device (%d)", ret);
+//		return ret;
+//	}
+//
+//    LOG_HEXDUMP_DBG(cmd_buf, sizeof(cmd_buf), "read");
+//    memcpy(val, cmd_buf, sizeof(cmd_buf));
+//
+//	return 0;
+//}
+
+
+static int abp2_status(const struct device *dev, uint8_t *status)
 {
-	const struct abp2_dev_config *cfg = dev->config;
+    const struct abp2_dev_config *cfg = dev->config;
 
-	uint8_t cmd_buf[7] = {0};
+    tx_bytes[0] = ABP2S_CMD_NOP;
+    tx_buf.len = 1;
+    rx_buf.len = 1;
 
-	const struct spi_buf rx_buf = { .buf = cmd_buf, .len = sizeof(cmd_buf) };
-	const struct spi_buf_set rx = { .buffers = &rx_buf, .count = 1 };
+    int ret = spi_transceive_dt(&cfg->bus, &pressure_tx_set, &pressure_rx_set);
 
-	int ret = spi_read_dt(&cfg->bus, &rx);
-
-	if (ret !=0) {
+    if (ret !=0) {
         LOG_ERR("Failed to read from SPI device (%d)", ret);
-		return ret;
-	}
+        return ret;
+    }
+    *status = rx_bytes[0];
 
-    LOG_HEXDUMP_DBG(cmd_buf, sizeof(cmd_buf), "read");
-    memcpy(val, cmd_buf, sizeof(cmd_buf));
-
-	return 0;
+    LOG_HEXDUMP_DBG(rx_bytes, sizeof(rx_bytes), "read");
+    return 0;
 }
 
 
@@ -46,15 +81,15 @@ static int abp2_sample_fetch(const struct device *dev, enum sensor_channel chan)
 		return -ENOTSUP;
 	}
 
-    uint8_t buf[7] = {0};
-    ret = abp2_read(dev, buf);
-    if (ret) {
-        return ret;
-    }
+//    uint8_t buf[7] = {0};
+//    ret = abp2_read(dev, buf);
+//    if (ret) {
+//        return ret;
+//    }
 
 
-    drv_data->pressure = (int32_t) sys_get_be24(&buf[1]);
-    drv_data->temperature = (int32_t ) sys_get_be24(&buf[4]);
+//    drv_data->pressure = (int32_t) sys_get_be24(&buf[1]);
+//    drv_data->temperature = (int32_t ) sys_get_be24(&buf[4]);
 
 	return 0;
 }
@@ -75,31 +110,22 @@ static int abp2_channel_get(const struct device *dev, enum sensor_channel chan,
 	return 0;
 }
 
-
-static int abp2_attr_set(const struct device *dev, enum sensor_channel chan,
-			    enum sensor_attribute attr, const struct sensor_value *val)
-{
-
-	return 0;
-}
-
 static int abp2_probe(const struct device *dev)
 {
-	int ret;
+    uint8_t status =0;
+    int ret = abp2_status(dev, &status);
+    if (ret < 0) {
+        LOG_ERR("Failed to read status byte from ABP2S device (%d)", ret);
+        return ret;
+    }
 
-    uint8_t  buf[7] = {0};
-    ret = abp2_read(dev, buf);
-	if (ret) {
-		return ret;
-	}
-    uint8_t status = buf[0];
     /* Device valid if Status == 01X0000X */
     if ((status & 0xDE) == 0x40)
     {
         LOG_ERR("Invalid status byte 0x%02x, want 01#0000#", status);
         return -ENODEV;
     }
-
+    LOG_DBG("Device status byte 0x%02x", status);
 	return 0;
 }
 
@@ -117,7 +143,7 @@ static int abp2_init(const struct device *dev)
 }
 
 static DEVICE_API(sensor, abp2_driver_api) = {
-	.attr_set = abp2_attr_set,
+//	.attr_set = abp2_attr_set,
 	.sample_fetch = abp2_sample_fetch, // Device to driver (private)
 	.channel_get  = abp2_channel_get, // driver to thread
 };
