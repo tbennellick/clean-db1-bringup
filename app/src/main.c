cce3dcef8d9a53ae72a25f23f368ab19de92cf44
Tom Bennellick <tom@bennellick.com>
2025-06-11 11:48:01 +0100

    Tidy up main.

--- app/src/main.c
+++ app/src/main.c
@@ -16,34 +16,23 @@ LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
 int main(void)
 {
-	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
+	LOG_INF("BFP2 Main core %s\n", APP_VERSION_STRING);
     init_debug_leds();
     debug_led_on();
     led_manager_init();
-    led_manager_set(LED_MANAGER_COLOUR_RED, LED_MANAGER_MODE_CONT);
-    k_sleep(K_MSEC(100));
-    led_manager_set(LED_MANAGER_COLOUR_GREEN, LED_MANAGER_MODE_CONT);
-    k_sleep(K_MSEC(100));
-    led_manager_set(LED_MANAGER_COLOUR_BLUE, LED_MANAGER_MODE_CONT);
-    k_sleep(K_MSEC(100));
-    led_manager_set(LED_MANAGER_COLOUR_WHITE, LED_MANAGER_MODE_CONT);
-    k_sleep(K_MSEC(100));
     led_manager_set(LED_MANAGER_COLOUR_OFF, LED_MANAGER_MODE_CONT);
-
     //    init_power(); Called early in kernel startup
 
     init_imu();
     init_rip();
     init_pressure();
     init_exg();
-
     gsm_init();
 
-    printk("Spam!....\n");
-
+    LOG_INF("Init complete");
 	while (1)
         {
-            k_sleep(K_MSEC(2000));
+            k_sleep(K_SECONDS(1));
             printk(".");
             debug_led_toggle();
         }
