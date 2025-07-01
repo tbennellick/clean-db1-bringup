dff99fefb884fee502a52263701605e1699d20cc
T Bennellick <tom@bennellick.com>
2025-07-01 12:11:39 +0100

    Format drver code.

--- drivers/sensor/veml6030/veml6030.c
+++ drivers/sensor/veml6030/veml6030.c
@@ -27,12 +27,10 @@ LOG_MODULE_REGISTER(VEML6030, CONFIG_SENSOR_LOG_LEVEL);
 #define VEML6030_IT_400 0x02
 #define VEML6030_IT_800 0x03
 
-#define VEML6030_GAIN_1 0x00
-#define VEML6030_GAIN_2 0x01
+#define VEML6030_GAIN_1     0x00
+#define VEML6030_GAIN_2     0x01
 #define VEML6030_GAIN_0_125 0x02
-#define VEML6030_GAIN_0_25 0x03
-
-
+#define VEML6030_GAIN_0_25  0x03
 
 struct veml6030_config {
 	struct i2c_dt_spec bus;
@@ -51,7 +49,6 @@ struct veml6030_data {
 	uint16_t white_counts;
 	uint32_t int_flags;
 };
-
 
 /* This fn in veml7700 drvier has a big comment about needing longer times. */
 __unused static void veml6030_sleep_by_integration_time(const struct veml6030_data *data)
@@ -86,17 +83,18 @@ static int veml6030_read(const struct device *dev, uint8_t cmd, uint16_t *data)
 	return 0;
 }
 
+static int veml6030_sample_fetch(const struct device *dev, enum sensor_channel chan)
+{
+	//    const struct veml6030_config *conf = dev->config;
+	//	struct veml6030_data *data = dev->data;
 
-static int veml6030_sample_fetch(const struct device *dev, enum sensor_channel chan) {
-//    const struct veml6030_config *conf = dev->config;
-//	struct veml6030_data *data = dev->data;
-
-        return 0;
+	return 0;
 }
 
-static int veml6030_channel_get(const struct device *dev, enum sensor_channel chan, struct sensor_value *val)
+static int veml6030_channel_get(const struct device *dev, enum sensor_channel chan,
+				struct sensor_value *val)
 {
-//	struct veml6030_data *data = dev->data;
+	//	struct veml6030_data *data = dev->data;
 
 	val->val1 = 0;
 	val->val2 = 0;
@@ -109,20 +107,20 @@ static int veml6030_channel_get(const struct device *dev, enum sensor_channel ch
 static int veml6030_pm_action(const struct device *dev, enum pm_device_action action)
 {
 	const struct veml6030_config *conf = dev->config;
-//
-//	if (conf->psm != VEML6030_PSM_DISABLED) {
-//		switch (action) {
-//		case PM_DEVICE_ACTION_SUSPEND:
-//			return veml6030_set_shutdown_flag(dev, 1);
-//
-//		case PM_DEVICE_ACTION_RESUME:
-//			return veml6030_set_shutdown_flag(dev, 0);
-//
-//		default:
-//			return -ENOTSUP;
-//		}
-//	}
-//
+	//
+	//	if (conf->psm != VEML6030_PSM_DISABLED) {
+	//		switch (action) {
+	//		case PM_DEVICE_ACTION_SUSPEND:
+	//			return veml6030_set_shutdown_flag(dev, 1);
+	//
+	//		case PM_DEVICE_ACTION_RESUME:
+	//			return veml6030_set_shutdown_flag(dev, 0);
+	//
+	//		default:
+	//			return -ENOTSUP;
+	//		}
+	//	}
+	//
 	return 0;
 }
 
@@ -131,7 +129,7 @@ static int veml6030_pm_action(const struct device *dev, enum pm_device_action ac
 static int veml6030_init(const struct device *dev)
 {
 	const struct veml6030_config *conf = dev->config;
-//	struct veml6030_data *data = dev->data;
+	//	struct veml6030_data *data = dev->data;
 
 	if (!i2c_is_ready_dt(&conf->bus)) {
 		LOG_ERR("Device not ready");
