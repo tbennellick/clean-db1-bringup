910b4eb48054555a386d3d9fd491afa5e89de102
T Bennellick <tom@bennellick.com>
2025-07-04 19:55:25 +0100

    Move audio RX to thread.

--- app/src/audio.c
+++ app/src/audio.c
@@ -12,9 +12,11 @@ LOG_MODULE_REGISTER(audio, LOG_LEVEL_INF);
 
 #define I2S_DEV_NODE_RX DT_ALIAS(i2s_node0)
 
+
+
+/* TODO: Think about this */
 #define NUM_BLOCKS 20
 #define SAMPLE_NO 64
-
 
 #define BLOCK_SIZE (2 * SAMPLE_NO)
 
@@ -32,8 +34,35 @@ STRUCT_SECTION_ITERABLE(k_mem_slab, rx_0_mem_slab) =
 				WB_UP(BLOCK_SIZE), NUM_BLOCKS + 2);
 
 
-#define TIMEOUT          2000
-#define FRAME_CLK_FREQ   8000
+
+#define RX_THREAD_STACK_SIZE 1024
+#define RX_THREAD_PRIORITY 4
+
+static struct k_thread rx_thread_data;
+K_THREAD_STACK_DEFINE(rx_thread_stack, RX_THREAD_STACK_SIZE);
+
+void rx_thread_func(void *p1, void *p2, void *p3)
+{
+    const struct device *dev_i2s = (const struct device *)p1;
+    void *rx_block;
+    size_t rx_size;
+    int ret;
+    
+    LOG_INF("RX thread started");
+    
+    while(1)
+    {
+        ret = i2s_read(dev_i2s, &rx_block, &rx_size);
+        if (ret < 0)
+        {
+            LOG_ERR("Failed to read I2S RX stream (%d)", ret);
+            return;
+        }
+//        LOG_INF("Received %d bytes from I2S RX stream", rx_size);
+
+        k_mem_slab_free(&rx_0_mem_slab, rx_block);
+    }
+}
 
 #define BRD_REV_62_2 /* Replace with board config from ENG-37*/
 #ifdef BRD_REV_62_2
@@ -95,7 +124,7 @@ static int configure_and_start_tx(const struct device *dev_i2s)
     i2s_cfg_tx.word_size = 16U;
     i2s_cfg_tx.channels = 2U;
     i2s_cfg_tx.format = I2S_FMT_DATA_FORMAT_I2S;
-    i2s_cfg_tx.frame_clk_freq = FRAME_CLK_FREQ;
+    i2s_cfg_tx.frame_clk_freq = SAMPLE_RATE;
     i2s_cfg_tx.block_size = BLOCK_SIZE;
     i2s_cfg_tx.timeout = TIMEOUT;
     i2s_cfg_tx.mem_slab = &tx_0_mem_slab;
@@ -141,8 +170,6 @@ static int configure_and_start_tx(const struct device *dev_i2s)
 
 void init_i2s(void)
 {
-    void *rx_block;
-    size_t rx_size;
     static const struct device *dev_i2s = DEVICE_DT_GET_OR_NULL(I2S_DEV_NODE_RX);
     struct i2s_config i2s_cfg_rx;
     int ret;
@@ -155,7 +182,7 @@ void init_i2s(void)
     i2s_cfg_rx.word_size = 16U;
     i2s_cfg_rx.channels = 2U;
     i2s_cfg_rx.format = I2S_FMT_DATA_FORMAT_I2S;
-    i2s_cfg_rx.frame_clk_freq = FRAME_CLK_FREQ;
+    i2s_cfg_rx.frame_clk_freq = SAMPLE_RATE;
     i2s_cfg_rx.block_size = BLOCK_SIZE;
     i2s_cfg_rx.timeout = TIMEOUT;
     i2s_cfg_rx.options = 0;
@@ -191,20 +218,60 @@ void init_i2s(void)
         return;
     }
 
+    /* Start RX thread to handle incoming audio data */
+    k_thread_create(&rx_thread_data, rx_thread_stack,
+                    K_THREAD_STACK_SIZEOF(rx_thread_stack),
+                    rx_thread_func,
+                    (void *)dev_i2s, NULL, NULL,
+                    RX_THREAD_PRIORITY, 0, K_NO_WAIT);
 
-    while(1)
+    LOG_INF("Audio system initialized - RX and TX threads running");
+
+}
+
+int init_audio(void) {
+
+    const struct device *const codec_dev = DEVICE_DT_GET(DT_NODELABEL(audio_codec));
+    struct audio_codec_cfg audio_cfg;
+
+    if (!device_is_ready(codec_dev))
     {
-        ret = i2s_read(dev_i2s, &rx_block, &rx_size);
-        if (ret < 0)
-        {
-            LOG_ERR("Failed to read I2S RX stream (%d)", ret);
-            return;
-        }
-        LOG_INF("Received %d bytes from I2S RX stream", rx_size);
-
-        k_mem_slab_free(&rx_0_mem_slab, rx_block);
+        LOG_ERR("%s is not ready", codec_dev->name);
+        return -ENODEV;
     }
 
+    audio_cfg.dai_route = AUDIO_ROUTE_CAPTURE;
+    audio_cfg.dai_type = AUDIO_DAI_TYPE_I2S;
+    audio_cfg.dai_cfg.i2s.word_size = 16;
+    audio_cfg.dai_cfg.i2s.channels = 2;
+    audio_cfg.dai_cfg.i2s.format = I2S_FMT_DATA_FORMAT_I2S;
+    audio_cfg.dai_cfg.i2s.options = I2S_OPT_FRAME_CLK_MASTER | I2S_OPT_BIT_CLK_MASTER;
+    audio_cfg.dai_cfg.i2s.frame_clk_freq = 8000;
+    audio_cfg.dai_cfg.i2s.mem_slab = NULL;
+    audio_cfg.dai_cfg.i2s.block_size = 0;
+
+    int ret = audio_codec_configure(codec_dev, &audio_cfg);
+    if (ret < 0)
+    {
+        LOG_ERR("Failed to configure audio codec: %d", ret);
+        return ret;
+    }
+
+    ret = audio_codec_route_input(codec_dev, AUDIO_CHANNEL_ALL, MAX9867_INPUT_MIC);
+    if (ret < 0)
+    {
+        LOG_ERR("Failed to route input: %d", ret);
+        return ret;
+    }
+    ret = audio_codec_set_property(codec_dev, AUDIO_PROPERTY_INPUT_VOLUME,
+                                   AUDIO_CHANNEL_ALL, (audio_property_value_t) {.vol = 10});
+    if (ret < 0)
+    {
+        LOG_ERR("Failed to set input volume: %d", ret);
+        return ret;
+    }
+    LOG_DBG("Audio codec configured successfully");
+    return 0;
 }
 
 int init_audio(void) {
