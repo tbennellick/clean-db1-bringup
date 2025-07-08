bb30b38e08f1125385df71fd507249cc7324862e
T Bennellick <tom@bennellick.com>
2025-07-08 22:49:40 +0100

    Refactor driver to use input mapping rather than channels.

--- drivers/audio/max9867/max9867.c
+++ drivers/audio/max9867/max9867.c
@@ -5,6 +5,7 @@
 #include <zephyr/sys/util.h>
 
 #include "max9867.h"
+#include "max9867_reg.h"
 #include "max9867_utils.h"
 
 #include <zephyr/logging/log.h>
@@ -63,7 +64,7 @@ static int set_mic_input_gain(const struct device *dev, audio_channel_t channel,
     uint8_t val = ((preamp_gain&0x3) << 5) | (mic_gain & 0x1f);
 
     uint8_t reg = MAX9867_MIC_GAIN_L;
-    if (channel == AUDIO_CHANNEL_REAR_RIGHT) {
+    if (channel == AUDIO_CHANNEL_FRONT_RIGHT) {
         reg = MAX9867_MIC_GAIN_R;
     }
     int ret = i2c_reg_write_byte_dt(&config->i2c, reg, val);
@@ -88,6 +89,67 @@ int set_registers_default(const struct device *dev)
     return 0;
 }
 
+int select_source(const struct device *dev, max9867_input_t input)
+{
+    const struct max9867_config *dev_cfg = dev->config;
+    struct max9867_data *data = dev->data;
+
+    int ret;
+
+    if (input > MAX9867_INPUT_MIC) {
+        LOG_ERR("Invalid input terminal: %d", input);
+        return -EINVAL;
+    }
+
+    if (data->input_source == input)
+    {
+        LOG_DBG("Input source already set to %d", input);
+        return 0;
+    }
+    data->input_source = input;
+
+    switch (input) {
+        case MAX9867_INPUT_LINE_IN:
+            LOG_DBG("Selecting line in");
+            ret = i2c_reg_write_byte_dt(&dev_cfg->i2c, MAX9867_SYS_SHDN,
+                                        MAX9867_SYS_SHDN_SHDN_nSHDN |
+                                        MAX9867_SYS_SHDN_SHDN_LNLEN |
+                                        MAX9867_SYS_SHDN_SHDN_LNREN);/* Todo: maybe this needs adlen/adren also*/
+            if (ret < 0) {
+                LOG_ERR("Failed to enable Line in: %d", ret);
+                return ret;
+            }
+            ret = i2c_reg_write_byte_dt(&dev_cfg->i2c, MAX9867_ADC_IN_CONF,
+                                        MAX9867_ADC_IN_CONF_LINE_ONLY);
+            if (ret < 0) {
+                LOG_ERR("Failed to set Mixer to Line only: %d", ret);
+                return ret;
+            }
+            break;
+        case MAX9867_INPUT_MIC:
+            LOG_DBG("Selecting mic in");
+            ret = i2c_reg_write_byte_dt(&dev_cfg->i2c, MAX9867_SYS_SHDN,
+                                        MAX9867_SYS_SHDN_SHDN_nSHDN |
+                                        MAX9867_SYS_SHDN_SHDN_ADLEN |
+                                        MAX9867_SYS_SHDN_SHDN_ADREN);
+            if (ret < 0) {
+                LOG_ERR("Failed to enable Mic in: %d", ret);
+                return ret;
+            }
+            ret = i2c_reg_write_byte_dt(&dev_cfg->i2c, MAX9867_ADC_IN_CONF,
+                                        MAX9867_ADC_IN_CONF_MIC_ONLY);
+            if (ret < 0) {
+                LOG_ERR("Failed to set Mixer to Mic only: %d", ret);
+                return ret;
+            }
+            break;
+        default:
+            LOG_ERR("Unsupported input: %d", input);
+            return -ENOTSUP;
+    }
+
+    return 0;
+}
 
 
 static int max9867_configure(const struct device *dev, struct audio_codec_cfg *cfg) {
@@ -123,7 +185,6 @@ static int max9867_configure(const struct device *dev, struct audio_codec_cfg *c
         return -EINVAL;
     }
 
-
     data->sample_rate = cfg->dai_cfg.i2s.frame_clk_freq;
 
     ret = set_registers_default(dev);
@@ -133,6 +194,7 @@ static int max9867_configure(const struct device *dev, struct audio_codec_cfg *c
         return ret;
     }
 
+    /* Setup clocks in "Normal" mode */
     uint8_t pre_scaler = 0;
     if (dev_cfg->mclk_rate < 10e6 || dev_cfg->mclk_rate > 20e6)
     {
@@ -143,150 +205,63 @@ static int max9867_configure(const struct device *dev, struct audio_codec_cfg *c
     {
         pre_scaler = MAX9867_SYS_CLK_PSCLK_10_20MHZ;
     }
-    uint32_t divisor = dev_cfg->mclk_rate/cfg->dai_cfg.i2s.frame_clk_freq;
 
-    ret = i2c_reg_write_byte_dt(&dev_cfg->i2c, MAX9867_SYS_CLK, pre_scaler);
+    ret = i2c_reg_write_byte_dt(&dev_cfg->i2c, MAX9867_SYS_CLK, pre_scaler); /* FREQ unused */
     if (ret < 0) {
         LOG_ERR("Failed to set system clock prescaler: %d", ret);
         return ret;
     }
 
+    uint32_t ni = calculate_ni(dev_cfg->mclk_rate, cfg->dai_cfg.i2s.frame_clk_freq);
 
 
+    ret = i2c_reg_write_byte_dt(&dev_cfg->i2c, MAX9867_SACLK_CTRL_HI, (ni >> 8)&MAX9867_SACLK_CTRL_HI_NI_MASK);
+    if (ret < 0) {
+        LOG_ERR("Failed to set SA Clock high: %d", ret);
+        return ret;
+    }
 
-    /*Set PLL bit 0*/
+    ret = i2c_reg_write_byte_dt(&dev_cfg->i2c, MAX9867_SACLK_CTRL_LO, ni & 0xff);
+    if (ret < 0) {
+        LOG_ERR("Failed to set SA Clock high: %d", ret);
+        return ret;
+    }
 
-//    ret = i2c_reg_update_byte_dt(&config->i2c, MAX9867_SYS_CLK, MAX9867_SYS_CLK_FREQ_MASK, 0x08 ); /* From DS Tab4*/
-//    if( ret < 0) {
-//        LOG_ERR("Failed to set fixed divider: %d", ret);
-//        return ret;
-//    }
-//
-//    ret = i2c_reg_update_byte_dt(&config->i2c, MAX9867_SYS_CLK, MAX9867_SYS_CLK_PLL_EN_BIT, 0);
-//    if (ret < 0) {
-//        LOG_ERR("Failed to disable PLL: %d", ret);
-//        return ret;
-//    }
-//    /* We don't set NI as we are using exact integer mode.*/
-//
-//    ret = i2c_reg_write_byte_dt(&config->i2c, MAX9867_DAI_IF_MODE1, MAX9867_DAI_IF_MODE1_MAS |
-//                                                                    MAX9867_DAI_IF_MODE1_HIZOFF);
-//    if (ret < 0) {
-//        LOG_ERR("Failed to set DAI mode1: %d", ret);
-//        return ret;
-//    }
-//
-//    ret = i2c_reg_write_byte_dt(&config->i2c, MAX9867_DAI_IF_MODE2, MAX9867_DAI_IF_MODE2_LVOLFIX |
-//                                                                    MAX9867_DAI_IF_MODE2_BSEL_NORM);
-//    if (ret < 0) {
-//        LOG_ERR("Failed to set DAI mode2: %d", ret);
-//        return ret;
-//    }
-//
-//    ret = i2c_reg_write_byte_dt(&config->i2c, MAX9867_CODEC_FILTER, 0); /* Disable Filters*/
-//    if (ret < 0) {
-//        LOG_ERR("Failed to disable filters: %d", ret);
-//        return ret;
-//    }
-//
-//    ret = i2c_reg_write_byte_dt(&config->i2c, MAX9867_SIDETONE, 0); /* Disable O/P mixer */
-//    if (ret < 0) {
-//        LOG_ERR("Failed to disable O/P mixer: %d", ret);
-//        return ret;
-//    }
-//
-//    ret = i2c_reg_write_byte_dt(&config->i2c, MAX9867_DAC_LEVEL, MAX9867_DAC_LEVEL_DACMUTE); /* Mute DAC */
-//    if (ret < 0) {
-//        LOG_ERR("Failed to Mute DAC: %d", ret);
-//        return ret;
-//    }
-//
-//    ret = i2c_reg_write_byte_dt(&config->i2c, MAX9867_ADC_LEVEL, MAX9867_ADC_LEVEL_0DB & (MAX9867_ADC_LEVEL_0DB<<4)); /* Set ADC to 0dB */
-//    if (ret < 0) {
-//        LOG_ERR("Failed to Set ADC gain: %d", ret);
-//        return ret;
-//    }
-//
-//    ret = i2c_reg_write_byte_dt(&config->i2c, MAX9867_LINE_IN_LEV_L, MAX9867_LINE_IN_LEV_X_MUTE);
-//    if (ret < 0) {
-//        LOG_ERR("Failed to Mute Line in L: %d", ret);
-//        return ret;
-//    }
-//
-//    ret = i2c_reg_write_byte_dt(&config->i2c, MAX9867_LINE_IN_LEV_R, MAX9867_LINE_IN_LEV_X_MUTE);
-//    if (ret < 0) {
-//        LOG_ERR("Failed to Mute Line in R: %d", ret);
-//        return ret;
-//    }
-//
-//    ret = i2c_reg_write_byte_dt(&config->i2c, MAX9867_VOL_L, MAX9867_VOL_X_MUTE);
-//    if (ret < 0) {
-//        LOG_ERR("Failed to Mute Playback volume L: %d", ret);
-//        return ret;
-//    }
-//
-//    ret = i2c_reg_write_byte_dt(&config->i2c, MAX9867_VOL_R, MAX9867_VOL_X_MUTE);
-//    if (ret < 0) {
-//        LOG_ERR("Failed to Mute Playback volume R: %d", ret);
-//        return ret;
-//    }
-//
-//    ret = i2c_reg_write_byte_dt(&config->i2c, MAX9867_MIC_GAIN_L, MAX9867_MIC_GAIN_X_PRE_0DB | MAX9867_MIC_GAIN_X_0DB);
-//    if (ret < 0) {
-//        LOG_ERR("Failed to Set MIC signal chain L: %d", ret);
-//        return ret;
-//    }
-//
-//    ret = i2c_reg_write_byte_dt(&config->i2c, MAX9867_MIC_GAIN_R, MAX9867_MIC_GAIN_X_PRE_0DB | MAX9867_MIC_GAIN_X_0DB);
-//    if (ret < 0) {
-//        LOG_ERR("Failed to Set MIC signal chain R: %d", ret);
-//        return ret;
-//    }
-//
-//    ret = i2c_reg_write_byte_dt(&config->i2c, MAX9867_ADC_IN_CONF, 0); /* Disable ADC Aux input and input mixer */
-//    if (ret < 0) {
-//        LOG_ERR("Failed to disable ADC input: %d", ret);
-//        return ret;
-//    }
-//
-//    ret = i2c_reg_write_byte_dt(&config->i2c, MAX9867_MIC_CONF, 0); /* Disable DMIC input */
-//    if (ret < 0) {
-//        LOG_ERR("Failed to disable DMIC input: %d", ret);
-//        return ret;
-//    }
-//
-//    /* TODO: Jack Det IRQ */
-//
-//    ret = i2c_reg_write_byte_dt(&config->i2c, MAX9867_MODE, 0); /* Disable jack features */
-//    if (ret < 0) {
-//        LOG_ERR("Failed to disable jack features: %d", ret);
-//        return ret;
-//    }
-//
-//    /* TODO: Enable Line in here with switching */
-//    ret = i2c_reg_write_byte_dt(&config->i2c, MAX9867_SYS_SHDN,  MAX9867_SYS_SHDN_SHDN_ADLEN | MAX9867_SYS_SHDN_SHDN_ADREN);
-//    if (ret < 0) {
-//        LOG_ERR("Failed to enable ADCs: %d", ret);
-//        return ret;
-//    }
+    /* Default for MAX9867_DAI_IF_MODE1*/
+    /* Default for MAX9867_DAI_IF_MODE2*/
+    /* Default for MAX9867_DAI_IF_MODE1*/
+    /* Default for MAX9867_CODEC_FILTER*/
+
+    /* Mute the DACs*/
+    ret = i2c_reg_write_byte_dt(&dev_cfg->i2c, MAX9867_DAC_LEVEL, MAX9867_DAC_LEVEL_DACMUTE);
+    if (ret < 0) {
+        LOG_ERR("Failed to Mute DAC: %d", ret);
+        return ret;
+    }
+
+
+    set_mic_input_gain(dev, AUDIO_CHANNEL_FRONT_LEFT, 0);
+    set_mic_input_gain(dev, AUDIO_CHANNEL_FRONT_RIGHT, 0);
+    set_line_input_gain(dev, AUDIO_CHANNEL_FRONT_LEFT, 0);
+    set_line_input_gain(dev, AUDIO_CHANNEL_FRONT_RIGHT, 0);
+
+    ret = i2c_reg_write_byte_dt(&dev_cfg->i2c, MAX9867_VOL_L, MAX9867_VOL_X_MUTE);
+    if (ret < 0) {
+        LOG_ERR("Failed to Mute Playback volume L: %d", ret);
+        return ret;
+    }
+
+    ret = i2c_reg_write_byte_dt(&dev_cfg->i2c, MAX9867_VOL_R, MAX9867_VOL_X_MUTE);
+    if (ret < 0) {
+        LOG_ERR("Failed to Mute Playback volume R: %d", ret);
+        return ret;
+    }
+
+    /* TODO: Jack Det IRQ ?*/
+
+    select_source(dev, MAX9867_INPUT_MIC); /* Default to Mic input */
 
     return 0;
-}
-
-__maybe_unused
-static int max9867_start_output(const struct device *dev)
-{
-	LOG_DBG("Starting MAX9867 output");
-	/* Implementation for starting output */
-	return 0;
-}
-
-__maybe_unused
-static int max9867_stop_output(const struct device *dev)
-{
-	LOG_DBG("Stopping MAX9867 output");
-	/* Implementation for stopping output */
-	return 0;
 }
 
 
@@ -294,28 +269,32 @@ __maybe_unused
 static int max9867_set_property(const struct device *dev,
 				 audio_property_t property, audio_channel_t channel,
 				 audio_property_value_t val) {
+
+    const struct max9867_config *dev_cfg = dev->config;
+    struct max9867_data *data = dev->data;
+
+
     switch (property)
     {
         case AUDIO_PROPERTY_INPUT_VOLUME:
-            if (channel == AUDIO_CHANNEL_FRONT_LEFT || channel == AUDIO_CHANNEL_FRONT_RIGHT)
-            {
-                return set_line_input_gain(dev, channel, val.vol);
-            }
-            else if (channel == AUDIO_CHANNEL_REAR_LEFT || channel == AUDIO_CHANNEL_REAR_RIGHT)
-            {
-                return set_mic_input_gain(dev, channel, val.vol);
-            }
-            else
-            {
-                LOG_ERR("Input volume setting %d not supported ", channel);
-                return -ENOTSUP;
-            }
             break;
         case AUDIO_PROPERTY_INPUT_MUTE:
             LOG_ERR("Property %d not implemented yet", property);
             return -ENOSYS;
         default:
             LOG_ERR("Property %d not supported", property);
+            return -ENOTSUP;
+    }
+
+    switch (data->input_source)
+    {
+        case MAX9867_INPUT_LINE_IN:
+            return set_line_input_gain(dev, channel, val.vol);
+        case MAX9867_INPUT_MIC:
+            return set_mic_input_gain(dev, channel, val.vol);
+        case MAX9867_INPUT_NONE:
+        default:
+            LOG_ERR("Input source not selected or unsupported: %d", data->input_source);
             return -ENOTSUP;
     }
 }
@@ -327,25 +306,52 @@ static int max9867_set_property(const struct device *dev,
 //	return 0;
 //}
 
+static int max9867_route_input(const struct device *dev, audio_channel_t channel, uint32_t input)
+{
+    struct max9867_config *dev_cfg = dev->config;
+    struct max9867_data *data = dev->data;
+
+    int ret;
+
+
+    if (channel != AUDIO_CHANNEL_FRONT_LEFT && channel != AUDIO_CHANNEL_FRONT_RIGHT) {
+        LOG_ERR("Invalid channel: %d", channel);
+        return -EINVAL;
+    }
+
+    ret = select_source(dev, input);
+    if (ret < 0) {
+        LOG_ERR("Failed to select source for channel %d: %d", channel, ret);
+        return ret;
+    }
+
+
+    return 0;
+}
+
 static const struct audio_codec_api max9867_driver_api = {
 	.configure = max9867_configure,
-//	.start_output = max9867_start_output,
+//	.start_output = max9867_start_output, /* Maybe these are required?*/
 //	.stop_output = max9867_stop_output,
 	.set_property = max9867_set_property,
-//        .route_input
+        .route_input = max9867_route_input,
 };
 
 static int max9867_init(const struct device *dev)
 {
-	const struct max9867_config *config = dev->config;
-	uint8_t revision;
+	const struct max9867_config *dev_cfg = dev->config;
+    struct max9867_data *data = dev->data;
+    uint8_t revision;
 	int ret;
 
-	if (!device_is_ready(config->i2c.bus)) {
-		LOG_ERR("I2C bus %s not ready", config->i2c.bus->name);
+	if (!device_is_ready(dev_cfg->i2c.bus)) {
+		LOG_ERR("I2C bus %s not ready", dev_cfg->i2c.bus->name);
 		return -ENODEV;
 	}
-	
+
+    data->input_source = MAX9867_INPUT_NONE;
+
+
 	ret = max9867_reg_read(dev, MAX9867_REVISION, &revision);
 	if (ret < 0) {
 		LOG_ERR("Failed to read revision register: %d", ret);
@@ -362,8 +368,8 @@ static int max9867_init(const struct device *dev)
 											\
 	static const struct max9867_config max9867_config_##inst = {			\
 		.i2c = I2C_DT_SPEC_INST_GET(inst),					\
-        .mclk_rate = DT_INST_PROP(inst, mclk_rate),        \
-        };										\
+        .mclk_rate = DT_INST_PROP(inst, mclk_rate), \
+        };								         \
 											\
 	DEVICE_DT_INST_DEFINE(inst, max9867_init, NULL,				\
 			      &max9867_data_##inst, &max9867_config_##inst,		\
---
+++ drivers/audio/max9867/max9867_reg.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/audio/codec.h>
+#include <zephyr/drivers/clock_control.h>
+
+#include "max9867.h"
+
+/* MAX9867 Register Addresses */
+typedef enum
+{
+    MAX9867_STATUS = 0x00,
+    MAX9867_JACK_STATUS = 0x01,
+    MAX9867_AUX_HIGH = 0x02,
+    MAX9867_AUX_LOW = 0x03,
+    MAX9867_INTERRUPT_ENABLE = 0x04,
+    MAX9867_SYS_CLK = 0x05,
+    MAX9867_SACLK_CTRL_HI = 0x06,
+    MAX9867_SACLK_CTRL_LO = 0x07,
+    MAX9867_DAI_IF_MODE1 = 0x08,
+    MAX9867_DAI_IF_MODE2 = 0x09,
+    MAX9867_CODEC_FILTER = 0x0A,
+    MAX9867_SIDETONE = 0x0B,
+    MAX9867_DAC_LEVEL = 0x0C,
+    MAX9867_ADC_LEVEL = 0x0D,
+    MAX9867_LINE_IN_LEV_L = 0x0E,
+    MAX9867_LINE_IN_LEV_R = 0x0F,
+    MAX9867_VOL_L = 0x10,
+    MAX9867_VOL_R = 0x11,
+    MAX9867_MIC_GAIN_L = 0x12,
+    MAX9867_MIC_GAIN_R = 0x13,
+    MAX9867_ADC_IN_CONF = 0x14,
+    MAX9867_MIC_CONF = 0x15,
+    MAX9867_MODE = 0x16,
+    MAX9867_SYS_SHDN = 0x17,
+    MAX9867_REVISION = 0xFF
+} max9867_reg_t;
+
+#define MAX9867_SYS_CLK_PSCLK_MASK (0x03<<4)
+#define MAX9867_SYS_CLK_PSCLK_OFF (0x00<<4)
+#define MAX9867_SYS_CLK_PSCLK_10_20MHZ (0x01<<4)
+#define MAX9867_SYS_CLK_FREQ_MASK (0x0F)
+#define MAX9867_SYS_CLK_PLL_EN_BIT (1<<7)
+#define MAX9867_SACLK_CTRL_HI_NI_MASK (0x7F)
+#define MAX9867_DAI_IF_MODE1_TDM_MODE_BIT (1<<2)
+#define MAX9867_DAI_IF_MODE1_MAS (1<<7)
+#define MAX9867_DAI_IF_MODE1_WCI (1<<6)
+#define MAX9867_DAI_IF_MODE1_BCI (1<<5)
+#define MAX9867_DAI_IF_MODE1_nSDODLY (1<<4)
+#define MAX9867_DAI_IF_MODE1_DLY (1<<3)
+#define MAX9867_DAI_IF_MODE1_HIZOFF (1<<2)
+#define MAX9867_DAI_IF_MODE2_LVOLFIX (1<<4)
+#define MAX9867_DAI_IF_MODE2_DMONO (1<<3)
+#define MAX9867_DAI_IF_MODE2_BSEL_MASK (0x07<<0)
+#define MAX9867_DAI_IF_MODE2_BSEL_NORM (0x02<<0)
+#define MAX9867_DAC_LEVEL_DACMUTE (1<<7)
+#define MAX9867_ADC_LEVEL_0DB (0x03<<0)
+#define MAX9867_LINE_IN_LEV_X_MUTE (1<<6)
+#define MAX9867_LINE_IN_LEV_X_GAIN_MASK 0x0F
+
+#define MAX9867_VOL_X_MUTE (1<<6)
+#define MAX9867_MIC_GAIN_X_PRE_MASK (0x3<<5)
+#define MAX9867_MIC_GAIN_X_PRE_0DB (0x01<<5)
+#define MAX9867_MIC_GAIN_X_0DB (0x14)
+#define MAX9867_ADC_IN_CONF_MIC_ONLY (1<<6)
+#define MAX9867_ADC_IN_CONF_LINE_ONLY (1<<7)
+
+#define MAX9867_SYS_SHDN_SHDN_nSHDN (1<<7)
+#define MAX9867_SYS_SHDN_SHDN_LNLEN (1<<6)
+#define MAX9867_SYS_SHDN_SHDN_LNREN (1<<5)
+#define MAX9867_SYS_SHDN_SHDN_DALEN (1<<3)
+#define MAX9867_SYS_SHDN_SHDN_DAREN (1<<2)
+#define MAX9867_SYS_SHDN_SHDN_ADLEN (1<<1)
+#define MAX9867_SYS_SHDN_SHDN_ADREN (1<<0)
+
+
+struct max9867_config {
+    struct i2c_dt_spec i2c;
+    uint32_t mclk_rate;
+};
+
+struct max9867_data {
+    uint32_t sample_rate;
+    max9867_input_t input_source;
+};
