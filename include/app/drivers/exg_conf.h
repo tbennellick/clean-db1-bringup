2129181f4cd8ddbe8610f943bb43e7d329431c0b
T Bennellick <tom@bennellick.com>
2025-07-21 15:57:42 +0100

    Move "subsystem" headers from drivers to include/

---
+++ include/app/drivers/exg_conf.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <stdint.h>
+struct exg_config {
+    /** Number of channels in use */
+    uint8_t channels;
+    /** Sample Rate */
+    uint32_t sample_clk_freq;
+    /** Memory slab to store RX data. */
+    struct k_mem_slab *mem_slab;
+    /** Read/Write timeout. Number of milliseconds to wait in case TX queue
+     * is full or RX queue is empty, or 0, or SYS_FOREVER_MS.
+     */
+    int32_t timeout;
+};
