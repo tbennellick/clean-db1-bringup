48676a48418a2d772a4ac40756ce8736f99ed453
T Bennellick <tom@bennellick.com>
2025-07-22 13:55:07 +0100

    Split out time calc function and add test cases.

--- drivers/i2s/ads1298/ads1298_reg.h
+++ drivers/i2s/ads1298/ads1298_reg.h
@@ -45,13 +45,4 @@
 #define ADS1298_CONFIG1_HR            BIT(7)
 
 
-/* ADS1298 Settling Time Table (Table 12 from datasheet) */
-typedef struct {
-    uint8_t dr_bits;         // DR[2:0] value (000 to 110)
-    uint16_t hr_mode_cycles; // High-Resolution Mode Settling Time (tCLK)
-    uint16_t lp_mode_cycles; // Low-Power Mode Settling Time (tCLK)
-} ads1298_settling_time_t;
-
-/* tCLK period in nanoseconds (488ps = 0.488ns) */
-#define ADS1298_TCLK_PS 488
 
