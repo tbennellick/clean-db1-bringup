ed0dce51520b0540c5cb25227ec1bffbb7bf77b6
T Bennellick <tom@bennellick.com>
2025-08-01 13:40:50 +0100

    Start converting to a thread and timer.

--- app/src/temperature.c
+++ app/src/temperature.c
@@ -14,29 +14,10 @@ LOG_MODULE_REGISTER(temperature, LOG_LEVEL_DBG);
 
 static const struct adc_dt_spec temp_adc_channel = ADC_DT_SPEC_GET_BY_IDX(DT_PATH(zephyr_user), 0);
 
-/* Todo: Unglobal?*/
-static int16_t m_sample_buffer[100];
-struct adc_sequence_options options;
-struct adc_sequence sequence;
 
 static struct k_msgq temp_msgq;
 static char __aligned(4) temp_msgq_buffer[TEMP_MSGQ_SIZE * sizeof(temp_block_t)];
 
-enum adc_action adc_callback(const struct device *dev,
-                                                 const struct adc_sequence *sequence,
-                                                 uint16_t sampling_index)
-{
-    debug_led_toggle(2);
-    LOG_DBG("ADC callback: sampling_index %d", sampling_index);
-    LOG_HEXDUMP_DBG(sequence->buffer, sequence->buffer_size, "ADC buffer");
-//    return ADC_ACTION_REPEAT;
-    if(sampling_index > 20) {
-        memset(m_sample_buffer, 0xaa, sizeof(m_sample_buffer));
-        return ADC_ACTION_REPEAT;
-    }
-    else
-        return ADC_ACTION_CONTINUE;
-}
 
 
 int init_temperature(void)
@@ -44,6 +25,8 @@ int init_temperature(void)
     int ret;
 
     LOG_DBG("Initializing temperature ADC");
+
+    k_msgq_init(&temp_msgq, temp_msgq_buffer, sizeof(temp_block_t), TEMP_MSGQ_SIZE);
 
     ret = adc_is_ready_dt(&temp_adc_channel);
     if (!ret) {
@@ -57,6 +40,11 @@ int init_temperature(void)
         return ret;
     }
 
+    static int16_t m_sample_buffer;
+    struct adc_sequence_options options;
+    struct adc_sequence sequence;
+
+
     memset(&sequence, 0, sizeof(sequence));
     memset(&options, 0, sizeof(options));
     memset(&m_sample_buffer, 0xaa, sizeof(m_sample_buffer));
@@ -68,22 +56,27 @@ int init_temperature(void)
         return ret;
     }
 
-    sequence.buffer = &m_sample_buffer;
-    sequence.buffer_size = sizeof(m_sample_buffer);
-    sequence.options = &options;
-    options.extra_samplings = 27;
-    options.interval_us = 800000;//CONFIG_NASAL_TEMP_SAMPLE_PERIOD;
-    options.callback = adc_callback;
-
-    /* Trigger the first read, continues based on return value in callback*/
-    ret = adc_read_async(temp_adc_channel.dev, &sequence, NULL);
-    if (ret < 0)
+    uint8_t sample_count =0;
+    temp_block_t block;
+    block.count = 0;
+    while(1)
     {
-        LOG_ERR("ADC async read failed: %d", ret);
-        return ret;
-    }
+        sequence.buffer = &block.samples[sample_count*2];
+        sequence.buffer_size = sizeof(int16_t);
+        ret = adc_read(temp_adc_channel.dev, &sequence);
+        if (ret < 0) {
+            LOG_ERR("ADC read failed: %d", ret);
+            return ret;
+        }
+        if(sample_count>= TEMP_BLOCK_SIZE)
+        {
+            sample_count = 0;
+            block.timestamp_ms =  k_uptime_get_32();
+            block.count++;
+            k_msgq_put(&temp_msgq, &block, K_NO_WAIT);
+        }
 
-    return 0;
+    }
 
 }
 
