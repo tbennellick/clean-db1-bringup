83cbd7c26c21759659fe57940cab2b02b731184e
Tom Bennellick <tom@bennellick.com>
2025-10-03 14:46:25 +0100

    Add back the other features in main.

--- app/src/main.c
+++ app/src/main.c
@@ -34,16 +34,16 @@ int main(void) {
 	led_manager_set(LED_MANAGER_COLOUR_OFF, LED_MANAGER_MODE_CONT);
 	//    init_power(); Called early in kernel startup
 
-	// init_imu();
-	// init_rip();
-	// init_pressure();
-	// //    init_exg();
-	// init_fuel_gauge();
-	// init_modem();
-	// init_als();
-	// init_audio();
-	// init_display();
-	// init_temperature();
+	init_imu();
+	init_rip();
+	init_pressure();
+	//    init_exg();
+	init_fuel_gauge();
+	init_modem();
+	init_als();
+	init_audio();
+	init_display();
+	init_temperature();
 	init_storage();
 
 	LOG_INF("Init complete");
