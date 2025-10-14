afa22ba0afafc06987de27559e3c6b4a31b9b312
Tom Bennellick <tom@bennellick.com>
2025-10-14 18:15:27 +0100

    Add skeleton message queue

---
+++ app/src/process_storage.c
@@ -0,0 +1,6 @@
+#include "process_storage.h"
+
+void process_storage_queue(struct k_msgq *msgq) {
+
+	while (1) {}
+}
--- app/src/storage.h
+++ app/src/storage.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <stdbool.h>
 
-int init_storage(void);
+struct k_msgq *init_storage(void);
 int storage_test(void);
 bool is_storage_ready(void);
 const char *get_storage_mount_point(void);
