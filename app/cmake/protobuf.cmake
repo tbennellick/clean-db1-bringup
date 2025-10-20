fd9f64465f019b4b02ca7548b8c3d0afc12d2a21
Tom Bennellick <tom@bennellick.com>
2025-10-20 10:27:24 +0100

    Revert the last commit.

--- app/cmake/protobuf.cmake
+++ app/cmake/protobuf.cmake
@@ -1,6 +1,7 @@
 # Protobuf (nanopb) configuration
 
-include(${ZEPHYR_NANOPB_MODULE_DIR}/cmake/nanopb.cmake)
+list(APPEND CMAKE_MODULE_PATH ${ZEPHYR_BASE}/modules/nanopb)
+include(nanopb)
 
 # Use protobuf definitions from bfp_common_sw module
 set(BFP_COMMON_SW_DIR ${ZEPHYR_BASE}/../bfp_common_sw)
